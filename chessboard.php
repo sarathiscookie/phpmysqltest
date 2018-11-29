feaf7ba9c8b6193e6ba437712d1e614a4a390f35
sarath <sarath@cabin-holiday.com>
2018-11-29 13:58:34 +0100

    Chess board create functionality

--- chessboard.php
+++ chessboard.php
@@ -15,47 +15,69 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             <h1 class="page-header">GD Library</h1>
 
+            <div class="col-sm-6">
+                <form action="" method="get">
+                    <div class="form-group">
+                        <label for="height">Height</label>
+                        <input type="number" name="height" id="height" class="form-control" placeholder="Height">
+                    </div>
+                    <div class="form-group">
+                        <label for="width">Width</label>
+                        <input type="number" name="width" id="width" class="form-control" placeholder="Width">
+                    </div>
+                    <div class="form-group">
+                        <label for="fieldWidth">Field Width</label>
+                        <input type="number" name="fieldWidth" id="fieldWidth" class="form-control" placeholder="Field Width">
+                    </div>
+                    <button type="submit" class="btn btn-default" name="generateImage">Generate Image</button>
+                </form>
+
+                <?php
+                if(isset($_REQUEST['generateImage'] )) {
+                    //header('Content-type: image/png');
+
+                    $imageCreate = @imagecreate(400, 400)
+                    or die("Cannot Initialize new GD image stream");
+                    $grey        = imagecolorallocate($imageCreate, 229, 229, 229);
+                    $black       = imagecolorallocate($imageCreate,0,0,0);
+                    $white       = imagecolorallocate($imageCreate,255,255,255);
+                    $fieldWidth  = (!empty($_REQUEST['fieldWidth'])) ? $_REQUEST['fieldWidth'] : 50;
+
+                    imagefilltoborder($imageCreate, 0, 0, $grey, $grey);
+
+                    imagefill($imageCreate,50,20, $white);
+
+                    for ($i = 1; $i < 8; $i++) {
+                        $cord = $i * $fieldWidth;
+                        imageline($imageCreate,0, $cord,400, $cord, $black);
+                        imageline($imageCreate, $cord,0, $cord,400, $black);
+                    }
+
+                    for ($i = 0; $i < 8; $i++)  {
+                        for ($j = 0; $j < 8; $j++)  {
+                            $x = ($i * $fieldWidth) + 2;
+                            $y = ($j * $fieldWidth) + 2;
+                            if ( (($i % 2) + ($j % 2) ) % 2 == 0 ) {
+                                imagefill($imageCreate, $x, $y, $black);
+                            }
+                        }
+                    }
+
+                    imagepng($imageCreate, 'img/chessboard.png');
+                    imagedestroy($imageCreate);
+                    ?>
+                    <h1>Image will find out in "img/chessboard.php" folder</h1>
+                    <img src="img/chessboard.png" alt="chessboard" width="400" height="400">
+                    <?php
+                }
+                ?>
+
+            </div>
 
         </div>
 
     </div>
 </div>
 
-<?php
-$tableWidth = 150 . 'px';
-$width      = 20 . 'px';
-$height     = 20 . 'px';
-
-$image = "<table width=".$tableWidth." style='margin-left: 200px;'>";
-for($i=0; $i < 8; $i++){
-    $image .= "<tr>";
-    for($j=0; $j < 8; $j++){
-        if($i % 2 == 0){
-            if($j % 2 == 0){
-                $image .= '<td style="background-color: pink; width: '.$width.'; height: '.$height.'"></td>';
-            } else {
-                $image .= '<td style="background-color: black; width: '.$width.'; height: '.$height.'"></td>';
-            }
-        } else {
-            if($j % 2 == 0){
-                $image .= '<td style="background-color: black; width: '.$width.'; height: '.$height.'"></td>';
-            } else {
-                $image .= '<td style="background-color: pink; width: '.$width.'; height: '.$height.'"></td>';
-            }
-        }
-    }
-    $image .= "</tr>";
-}
-$image .= "<table>";
-
-$im               = @imagecreate(300, 600)
-or die("Cannot Initialize new GD image stream");
-$background_color = imagecolorallocate($im, 0, 0, 0);
-$text_color       = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  $image, $text_color);
-//imagettftext($img, 9, 0, 1, 1, $white, "VERDANA.TTF", $html_code);
-header("Content-Type: image/png");
-imagepng($im, 'img/chessboard.png');
-?>
 <!-- Footer -->
 <?php require 'includes/footer.php'; ?>
